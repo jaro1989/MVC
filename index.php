f1d4e1d3842cd2387c0a03a5a30553fab24662a5
Vitaly <vmentuz89@gmail.com>
2014-08-09 08:36:23 +0300

    first controller

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
+define('DEFAULT_CONTROLLER','main');
 
+$url = isset($_GET['url'])? trim($_GET['url']): DEFAULT_CONTROLLER;
 
+$controllerFileName = 'inc/controllers/'.$url.'.php';
+$controllerName = $url.'Controller';
+
+require_once($controllerFileName);
+$controller = new $controllerName;
